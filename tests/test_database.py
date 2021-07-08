8b7077c813dd99a1dea1e1aa1f6a5ef6fbf8b486
Aidan Payne <aidan@apayne.net>
2021-07-08 12:20:48 -0700

    Added test for createDatabase()
    Modified test class definitions for mock sqlite cursor and connection objects

--- tests/test_database.py
+++ tests/test_database.py
@@ -2,22 +2,29 @@ import sqlite3
 from _pytest.monkeypatch import monkeypatch
 import pytest
 import utils.database as database
-from sqlite3 import OperationalError, Cursor
+
+class mockConnection:
+    def __init__(self, data=None):
+        self.data = data
+    def cursor(self,*args, **kwargs):
+        return mockCursor(self.data)
 
 class mockCursor:
-        @classmethod
-        def execute(*args, **kwargs):
-            return None
+    def __init__(self, data=None):
+        self.data = data
+    def execute(self, *args, **kwargs):
+        return self.data
+
 
 class Test_addGame_Unit:
-    @pytest.mark.parametrize("test_cur,test_name,test_player, expected", [(mockCursor, "Pong", "Terlen",None)])
+    @pytest.mark.parametrize("test_cur,test_name,test_player, expected", [(mockCursor(), "Pong", "Terlen",None)])
     def test_addGame(self, test_cur, test_name, test_player, expected):
         # successful test case
         # input: cursor, game name, player
         # output: None
         assert database.addGame(test_cur, test_name, test_player) == expected
 
-    @pytest.mark.parametrize("test_cur,test_name,test_player, expected", [(mockCursor, "Pong", "Terlen",None)])
+    @pytest.mark.parametrize("test_cur,test_name,test_player, expected", [(mockCursor(), "Pong", "Terlen",None)])
     def test_addGameOperationalError(self, monkeypatch, test_cur, test_name, test_player, expected):
         # failure test case
         # input: cursor, game name, player
@@ -28,7 +35,7 @@ class Test_addGame_Unit:
         with pytest.raises(sqlite3.OperationalError):
             assert database.addGame(test_cur, test_name, test_player) == expected
 
-    @pytest.mark.parametrize("test_cur,test_name,test_player, expected", [(mockCursor, "Pong", "Fake",None)])
+    @pytest.mark.parametrize("test_cur,test_name,test_player, expected", [(mockCursor(), "Pong", "Fake",None)])
     def test_addGameIntegrityError(self, monkeypatch, test_cur, test_name, test_player, expected):
         # failure test case
         # input: cursor,  game name, player who doesn't yet exist in the players table
@@ -39,6 +46,17 @@ class Test_addGame_Unit:
         with pytest.raises(sqlite3.IntegrityError):
             assert database.addGame(test_cur, test_name, test_player) == expected
 
+class Test_createDatabase_Unit:
+    # success test case
+    # input: guildID: int
+    # output: Tuple[str, ...]
+    @classmethod
+    def mock_Connect(self,*args, **kwargs):
+            return mockConnection(('games','players',))
+
+    def test_createDatabase(self, monkeypatch):
+        monkeypatch.setattr(sqlite3, "connect", self.mock_Connect)
+        assert database.createDatabase(1234) == ('games','players',)
 
 def test_addPlayer():
     pass
