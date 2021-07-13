dcc0d6b78a62fa34f6e5e427b53e217ac9a951f2
Aidan Payne <aidan@apayne.net>
2021-07-12 17:01:16 -0700

    Added tests for initializeDatabase
    Moved mock_Connect to wider scope for reuse

--- tests/test_database.py
+++ tests/test_database.py
@@ -22,7 +22,13 @@ def mock_OperationalError(*args, **kwargs):
 
 def mock_IntegrityError(*args, **kwargs):
             raise sqlite3.IntegrityError("Violation of foreign key requirement")
-    
+
+def mock_ProgrammingError(*args, **kwargs):
+            raise sqlite3.ProgrammingError("Table already exists or not found")
+
+def mock_Connect(self,*args, **kwargs):
+       return mockConnection()
+
 class Test_addGame_Unit:
     @pytest.mark.parametrize("cursor, name, player, expected", [(mockCursor(), "Pong", "Terlen", None)])
     def test_addGame(self, cursor, name, player, expected):
@@ -50,16 +56,12 @@ class Test_addGame_Unit:
             assert database.addGame(cursor, name, player) == expected
 
 class Test_Unit_dbConnect:
-    
-    def mock_Connect(self,*args, **kwargs):
-       return mockConnection()
-
     # success test case
     # input: guildID: int
     # output: sqlite3.Connection
     @pytest.mark.parametrize("guildID", [(1234)])
     def test_dbConnect(self, monkeypatch, guildID):
-        monkeypatch.setattr(sqlite3, "connect", self.mock_Connect)
+        monkeypatch.setattr(sqlite3, "connect", mock_Connect)
         assert isinstance(database.dbConnect(guildID), mockConnection)
 
     # failure test case: unexpected disconnect
@@ -128,3 +130,102 @@ class Test_Unit_dbCreateGameTable:
         monkeypatch.setattr(cursor, "execute", mock_IntegrityError)
         with pytest.raises(sqlite3.IntegrityError):
             assert database.dbCreateGameTable(connection, cursor) is None
+
+class Test_Unit_initializeDatabase:
+    # Successful test case
+    # input: guildID: int
+    # output: sqlite3.connection
+    @pytest.mark.parametrize("guildID", [(1234)])
+    def test_initializeDatabase(self, monkeypatch, guildID):
+        monkeypatch.setattr(sqlite3, "connect", mock_Connect)
+        monkeypatch.setattr(database, "dbCreatePlayersTable", lambda *args, **kwargs: None)
+        monkeypatch.setattr(database, "dbCreateGameTable", lambda *args, **kwargs: None)
+        assert isinstance(database.initializeDatabase(guildID), mockConnection)
+
+    # Failure test case: OperationalError when creating db file
+    # input: guildID: int
+    # raise: sqlite3.OperationalError
+    @pytest.mark.parametrize("guildID", [(1234)])
+    def test_initializeDatabase_OperationalError_dbConnect(self, monkeypatch, guildID):
+        monkeypatch.setattr(sqlite3, "connect", mock_OperationalError)
+        monkeypatch.setattr(database, "dbCreatePlayersTable", lambda *args, **kwargs: None)
+        monkeypatch.setattr(database, "dbCreateGameTable", lambda *args, **kwargs: None)
+        with pytest.raises(sqlite3.OperationalError):
+            assert isinstance(database.initializeDatabase(guildID), mockConnection)
+    
+    # Failure test case: IntegrityError when creating db file
+    # input: guildID: int
+    # raise: sqlite3.IntegrityError
+    @pytest.mark.parametrize("guildID", [(1234)])
+    def test_initializeDatabase_IntegrityError_dbConnect(self, monkeypatch, guildID):
+        monkeypatch.setattr(sqlite3, "connect", mock_IntegrityError)
+        monkeypatch.setattr(database, "dbCreatePlayersTable", lambda *args, **kwargs: None)
+        monkeypatch.setattr(database, "dbCreateGameTable", lambda *args, **kwargs: None)
+        with pytest.raises(sqlite3.IntegrityError):
+            assert isinstance(database.initializeDatabase(guildID), mockConnection)
+    
+    # Failure test case: OperationalError when creating players table
+    # input: guildID: int
+    # raise: sqlite3.IntegrityError
+    @pytest.mark.parametrize("guildID", [(1234)])
+    def test_initializeDatabase_OperationalError_createPlayers(self, monkeypatch, guildID):
+        monkeypatch.setattr(sqlite3, "connect", mock_Connect)
+        monkeypatch.setattr(database, "dbCreatePlayersTable", mock_OperationalError)
+        monkeypatch.setattr(database, "dbCreateGameTable", lambda *args, **kwargs: None)
+        with pytest.raises(sqlite3.OperationalError):
+            assert isinstance(database.initializeDatabase(guildID), mockConnection)
+    
+    # Failure test case: IntegrityError when creating players table
+    # input: guildID: int
+    # raise: sqlite3.IntegrityError
+    @pytest.mark.parametrize("guildID", [(1234)])
+    def test_initializeDatabase_IntegrityError_createPlayers(self, monkeypatch, guildID):
+        monkeypatch.setattr(sqlite3, "connect", mock_Connect)
+        monkeypatch.setattr(database, "dbCreatePlayersTable", mock_IntegrityError)
+        monkeypatch.setattr(database, "dbCreateGameTable", lambda *args, **kwargs: None)
+        with pytest.raises(sqlite3.IntegrityError):
+            assert isinstance(database.initializeDatabase(guildID), mockConnection)
+
+    # Failure test case: ProgrammingError when creating players table
+    # input: guildID: int
+    # raise: sqlite3.ProgrammingError
+    @pytest.mark.parametrize("guildID", [(1234)])
+    def test_initializeDatabase_ProgrammingError_createPlayers(self, monkeypatch, guildID):
+        monkeypatch.setattr(sqlite3, "connect", mock_Connect)
+        monkeypatch.setattr(database, "dbCreatePlayersTable", mock_ProgrammingError)
+        monkeypatch.setattr(database, "dbCreateGameTable", lambda *args, **kwargs: None)
+        with pytest.raises(sqlite3.ProgrammingError):
+            assert isinstance(database.initializeDatabase(guildID), mockConnection)
+    
+    # Failure test case: OperationalError when creating game table
+    # input: guildID: int
+    # raise: sqlite3.OperationalError
+    @pytest.mark.parametrize("guildID", [(1234)])
+    def test_initializeDatabase_OperationalError_createGameTable(self, monkeypatch, guildID):
+        monkeypatch.setattr(sqlite3, "connect", mock_Connect)
+        monkeypatch.setattr(database, "dbCreatePlayersTable", lambda *args, **kwargs: None)
+        monkeypatch.setattr(database, "dbCreateGameTable", mock_OperationalError)
+        with pytest.raises(sqlite3.OperationalError):
+            assert isinstance(database.initializeDatabase(guildID), mockConnection)
+
+    # Failure test case: IntegrityError when creating game table
+    # input: guildID: int
+    # raise: sqlite3.IntegrityError
+    @pytest.mark.parametrize("guildID", [(1234)])
+    def test_initializeDatabase_IntegrityError_createGameTable(self, monkeypatch, guildID):
+        monkeypatch.setattr(sqlite3, "connect", mock_Connect)
+        monkeypatch.setattr(database, "dbCreatePlayersTable", lambda *args, **kwargs: None)
+        monkeypatch.setattr(database, "dbCreateGameTable", mock_IntegrityError)
+        with pytest.raises(sqlite3.IntegrityError):
+            assert isinstance(database.initializeDatabase(guildID), mockConnection)
+    
+    # Failure test case: ProgrammingError when adding creating game table
+    # input: guildID: int
+    # raise: sqlite3.ProgrammingError
+    @pytest.mark.parametrize("guildID", [(1234)])
+    def test_initializeDatabase_ProgrammingError_createGameTable(self, monkeypatch, guildID):
+        monkeypatch.setattr(sqlite3, "connect", mock_Connect)
+        monkeypatch.setattr(database, "dbCreatePlayersTable", lambda *args, **kwargs: None)
+        monkeypatch.setattr(database, "dbCreateGameTable", mock_ProgrammingError)
+        with pytest.raises(sqlite3.ProgrammingError):
+            assert isinstance(database.initializeDatabase(guildID), mockConnection)
